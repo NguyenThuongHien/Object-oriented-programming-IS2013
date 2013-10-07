d3f5b26298f5535ff8a58a33b0664927f490a570
wukat <wojciechksprk@gmail.com>
2013-10-07 23:32:48 +0200

    next exercises

--- lab1/src/lab1/HelloWorld.java
+++ lab1/src/lab1/HelloWorld.java
@@ -1,13 +1,12 @@
 package lab1;
 
 import javaIn.JIn;
- 
-public class HelloWorld {
-   public static void main(String [] argv){
-     System.out.print("Type your name: ");
-	 String name = JIn.getString();
-     System.out.println("Hello World");
-	 System.out.println("And hello "+name);
-  }
-}     
 
+public class HelloWorld {
+	public static void main(String[] argv) {
+		System.out.print("Type your name: ");
+		String name = JIn.getString();
+		System.out.println("Hello World");
+		System.out.println("And hello " + name);
+	}
+}
