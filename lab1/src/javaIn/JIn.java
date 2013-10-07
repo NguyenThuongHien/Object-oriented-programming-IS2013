b3cfe6f5739ab9df0f633b63b66fe9d8687a140f
wukat <wojciechksprk@gmail.com>
2013-10-07 23:55:13 +0200

    added getInteger()

--- lab1/src/javaIn/JIn.java
+++ lab1/src/javaIn/JIn.java
@@ -1,15 +1,31 @@
 package javaIn;
+
 import java.io.*;
- 
+
 public class JIn {
-    public static String getString() {
-	  String text = null;
-	  try{
-		InputStreamReader rd = new InputStreamReader(System.in);
-		BufferedReader bfr = new BufferedReader(rd);
- 
-		text = bfr.readLine();
-	  }catch(IOException e){e.printStackTrace();}
-      return text;	  
-    }
+	public static String getString() {
+		String text = null;
+		try {
+			InputStreamReader rd = new InputStreamReader(System.in);
+			BufferedReader bfr = new BufferedReader(rd);
+
+			text = bfr.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return text;
+	}
+
+	public static Integer getInteger() {
+		int a = 0;
+		try {
+			InputStreamReader rd = new InputStreamReader(System.in);
+			BufferedReader bfr = new BufferedReader(rd);
+			
+			a = Integer.parseInt(bfr.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return a;
+	}
 }
