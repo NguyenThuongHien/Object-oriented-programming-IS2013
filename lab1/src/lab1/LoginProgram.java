7f5eddb627a0c4aef963cca8e0ead55ab8155bc0
wukat <wojciechksprk@gmail.com>
2013-10-07 23:34:38 +0200

    next exercises

---
+++ lab1/src/lab1/LoginProgram.java
@@ -0,0 +1,43 @@
+package lab1;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+
+/**
+ * 
+ * @author Szymon
+ */
+public class LoginProgram {
+	public static void main(String[] argv) {
+		Login log = new Login("ala", "makota");
+		try {
+			InputStreamReader rd = new InputStreamReader(System.in);
+			BufferedReader bfr = new BufferedReader(rd);
+
+			// prosba o wpisanie hasła i loginu
+			System.out.println("Podaj login:");
+			String login = bfr.readLine();
+			System.out.println("Podaj hasło:");
+			String haslo = bfr.readLine();
+
+			/*
+			 * sprawdzenie czy podane hasło i login zgadzaja sie z tymi
+			 * przechowywanymi w obiekcie log Jeśli tak, to ma zostać
+			 * wyswietlone "OK", jesli nie - prosze wyswietlić informacje o
+			 * błedzie
+			 */
+			if (log.check(login, haslo))
+				System.out.println("OK");
+			else
+				System.out.println("Bledne dane");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+/* TODO javadoc */
---
+++ lab1/src/lab1/WitajSwiecie.java
@@ -0,0 +1,20 @@
+/*
+ *
+ * Klasa wyświetlajaca powitaniena konsoli.
+ * @author Szymon
+ */
+package lab1;
+
+public class WitajSwiecie {
+	/**
+	 * 
+	 * @param argv
+	 *            lista argumentów przekazywana do programu
+	 */
+	public static void main(String[] argv) {
+		System.out.println("Hello World!");
+	}
+}
+
+// bledna nazwa pliku/klasy
+// brak paczuszki
