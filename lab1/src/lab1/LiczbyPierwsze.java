263dd0d5ea73790907ad0fffd8e47c511fbb2268
wukat <wojciechksprk@gmail.com>
2013-10-08 10:03:56 +0200

    changed bracket

--- lab1/src/lab1/LiczbyPierwsze.java
+++ lab1/src/lab1/LiczbyPierwsze.java
@@ -3,35 +3,34 @@ package lab1;
 /**
  * @brief Class finding prim's numbers
  * @author wukat
- *
+ * 
  */
 public class LiczbyPierwsze {
-	private Integer n; /** n - integer, founded prim's will be smaller than it **/
-	
+	private Integer n; // n - integer, founded prim's will be smaller than it
+
 	/**
 	 * @brief Constructor
 	 * @param _n
 	 */
-    public LiczbyPierwsze(Integer _n) {
-    	n = _n;
-    }
-    
-    /**
-     * @brief Function founds and prints prim's numbers
-     */
-    public void znajdz() {
-    	int j = 2;
-    	for (int i = 2; i < n; i++) {
-    		boolean flag = true;
-    		j = 2;
-    	    while (j * j <= i && flag) {
-    		    if (i % j == 0)
-    		    	flag = false;
-    		    j++;
-    	    }
-    		if (flag)
-    			System.out.println(i);
-    	}			
-    }
-    
+	public LiczbyPierwsze(Integer _n) {
+		n = _n;
+	}
+
+	/**
+	 * @brief Function founds and prints prim's numbers
+	 */
+	public void znajdz() {
+		int j = 2;
+		for (int i = 2; i < n; i++) {
+			boolean flag = true;
+			j = 2;
+			while (j * j <= i && flag) {
+				if (i % j == 0)
+					flag = false;
+				j++;
+			}
+			if (flag)
+				System.out.println(i);
+		}
+	}
 }
