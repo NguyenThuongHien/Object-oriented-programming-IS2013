040a366bc819bcbfd4073474c2716baab91c1fe6
wukat <wojciechksprk@gmail.com>
2013-10-10 22:33:53 +0200

    third class to play with extending

---
+++ labs/src/pcg2/C.java
@@ -0,0 +1,22 @@
+/**
+ * C.java
+ * @author - wukat
+ * @data - 10 paź 2013
+ */
+package pcg2;
+
+import pcg1.B;
+
+/**
+ * @author wukat
+ * 
+ */
+public class C extends B {
+	public C(int argNumber, String argName) {
+		super(argNumber, argName);
+	}
+
+	void changeName() {
+    //	name = 'Cinne'; nie widzi zmiennej; package nie idzie z dziedziczeniem poza paczkę
+    }
+}
