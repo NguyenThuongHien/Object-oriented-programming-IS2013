2e70624b0255315a4295f6ce631edf46216cd0ce
wukat <wojciechksprk@gmail.com>
2013-10-12 23:19:52 +0200

    Entry class - implementation of entry in crossword

---
+++ crossword/src/dictionary/Entry.java
@@ -0,0 +1,67 @@
+/**
+ * Entry.java
+ * @author - wukat
+ * @data - 12 paź 2013
+ */
+package dictionary;
+
+/**
+ * @author wukat
+ * 
+ */
+public class Entry {
+	private String word;
+	private String clue;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param word
+	 *            - word in crossword
+	 * @param clue
+	 *            - clue in crossword
+	 */
+	public Entry(String word, String clue) {
+		this.word = word;
+		this.clue = clue;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the word
+	 */
+	public String getWord() {
+		return word;
+	}
+
+	/**
+	 * word setter
+	 * 
+	 * @param word
+	 *            the word to set
+	 */
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the clue
+	 */
+	public String getClue() {
+		return clue;
+	}
+
+	/**
+	 * clue setter
+	 * 
+	 * @param clue
+	 *            the clue to set
+	 */
+	public void setClue(String clue) {
+		this.clue = clue;
+	}
+
+}
