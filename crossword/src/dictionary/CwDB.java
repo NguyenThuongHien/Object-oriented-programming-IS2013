9d209f48de9392f0269ca2e6486109230f7ab313
wukat <wojciechksprk@gmail.com>
2013-10-13 00:13:00 +0200

    Crossword database

---
+++ crossword/src/dictionary/CwDB.java
@@ -0,0 +1,132 @@
+/**
+ * CwDB.java
+ * @author - wukat
+ * @data - 12 paź 2013
+ */
+package dictionary;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+
+/**
+ * Implementation of crossword database
+ * 
+ * @author wukat
+ * 
+ */
+public class CwDB {
+	private LinkedList<Entry> dict; // dictionary - list of entries
+
+	/**
+	 * Constructor
+	 * 
+	 * @param filename
+	 *            - name of file with data
+	 */
+	public CwDB(String filename) {
+		try {
+			createDB(filename);
+		} catch (FileNotFoundException e) {
+			System.out.println("Nie ma takiego pliku!");
+			// TODO something here
+		} catch (IOException e) {
+			// TODO something here
+		}
+	}
+
+	/**
+	 * Adds new record
+	 * 
+	 * @param word
+	 * @param clue
+	 */
+	public void add(String word, String clue) {
+		dict.add(new Entry(word, clue));
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @param word
+	 *            - parameter to identify entry
+	 * @return entry in dictionary with field word
+	 */
+	public Entry get(String word) {
+		for (int i = 0; i < getSize(); i++) {
+			if (dict.get(i).getWord().equals(word)) {
+				return dict.get(i);
+			}
+		}
+		return new Entry("", "");
+	}
+
+	/**
+	 * Removes specified entry from dictionary
+	 * 
+	 * @param word
+	 *            - parameter to identify entry
+	 */
+	public void remove(String word) {
+		for (int i = 0; i < getSize(); i++) {
+			if (dict.get(i).getWord().equals(word)) {
+				dict.remove(i);
+			}
+		}
+	}
+
+	/**
+	 * Saves database in file
+	 * 
+	 * @param filename
+	 *            - name of output file
+	 */
+	public void saveDB(String filename) throws IOException {
+		ObjectOutputStream outputDB = null;
+		try {
+			outputDB = new ObjectOutputStream(new FileOutputStream(filename));
+			for (int i = 0; i < getSize(); i++) {
+				outputDB.writeChars(dict.get(i).toString());
+			}
+		} finally {
+			if (outputDB != null)
+				outputDB.close();
+		}
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return size of dictionary (number of elements)
+	 */
+	public int getSize() {
+		return dict.size();
+	}
+
+	/**
+	 * Creates database from file
+	 * 
+	 * @param filename
+	 *            - file name
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	protected void createDB(String filename) throws IOException,
+			FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		try {
+			String line1 = null;
+			String line2 = null;
+			while (((line1 = br.readLine()) != null)
+					&& ((line2 = br.readLine()) != null)) {
+				add(line1, line2);
+			}
+		} finally {
+			br.close();
+		}
+	}
+}
