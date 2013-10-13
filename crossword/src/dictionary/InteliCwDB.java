6d09f3070a4fb6819792a16c66f647fd40ad6475
wukat <wojciechksprk@gmail.com>
2013-10-13 10:39:28 +0200

    Intelligent crossword database class added

---
+++ crossword/src/dictionary/InteliCwDB.java
@@ -0,0 +1,43 @@
+/**
+ * InteliCwDB.java
+ * @author - wukat
+ * @data - 13 paź 2013
+ */
+package dictionary;
+
+import java.util.LinkedList;
+
+/**
+ * Intelligent crossword database
+ * @author wukat
+ *
+ */
+public class InteliCwDB extends CwDB {
+
+	/**
+	 * Constructor
+	 * @param filename
+	 */
+	public InteliCwDB(String filename) {
+		super(filename);
+		// TODO Auto-generated constructor stub
+	}
+	
+//	public LinkedList<Entry> findAll(String pattern) {
+//		
+//	}
+//	
+//	public Entry getRandom() {}
+//	
+//	public Entry getRandom(int length) {
+//		
+//	}
+//	
+//	public Entry getRandom(String pattern) {
+//		
+//	}
+//	
+//	public void add(String word, String clue) {
+//		
+//	}
+}
