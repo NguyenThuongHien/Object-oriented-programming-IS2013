b61c726ebe75831c99b5c64a1f1eb88facdf3121
wukat <wojciechksprk@gmail.com>
2013-10-31 21:38:01 +0100

    interface

---
+++ crossword/src/browser/Reader.java
@@ -0,0 +1,17 @@
+/**
+ * Reader.java
+ * @author - wukat
+ * @data - 31 paź 2013
+ */
+package browser;
+
+/**
+ * @author wukat
+ *
+ */
+interface Reader {
+	/**
+	 * Loads all crosswords into one data base.
+	 */
+    public void getAllCws();
+}
