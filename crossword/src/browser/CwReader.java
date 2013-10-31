794ef503d20658193f3728b0653ad9c93d8ed64e
wukat <wojciechksprk@gmail.com>
2013-10-31 21:37:49 +0100

    implementation of Reader interface, TODO.

---
+++ crossword/src/browser/CwReader.java
@@ -0,0 +1,47 @@
+/**
+ * CwReader.java
+ * @author - wukat
+ * @data - 31 paź 2013
+ */
+package browser;
+
+import java.io.File;
+import java.util.Vector;
+
+import board.Crossword;
+
+/**
+ * @author wukat
+ * 
+ */
+public class CwReader implements Reader {
+	private Vector<Crossword> crosswords;
+	private File file;
+
+	/**
+	 * 
+	 * Constructor
+	 * 
+	 * @param path
+	 *            - folder with crosswords
+	 */
+	public CwReader(String path) {
+		file = new File(path);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see browser.Reader#getAllCws()
+	 */
+	@Override
+	public void getAllCws() {
+		for (File f : file.listFiles()) {
+			if (f.canRead()) {
+				//TODO reading from file (i don't know the format
+				//crosswords.add(new Crossword(Integer.parseInt(f.getName()), ....)
+			}
+		}
+	}
+
+}
