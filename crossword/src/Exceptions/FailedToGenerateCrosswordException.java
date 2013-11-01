2ce0d48548e9e3edaecfbe8e02dad88efe5ec69a
wukat <wojciechksprk@gmail.com>
2013-11-01 13:15:37 +0100

    Exception class (wtf?)

---
+++ crossword/src/Exceptions/FailedToGenerateCrosswordException.java
@@ -0,0 +1,64 @@
+/**
+ * FailedToGnerateCrosswordException.java
+ * @author - wukat
+ * @data - 1 lis 2013
+ */
+package Exceptions;
+
+/**
+ * @author wukat
+ *
+ */
+public class FailedToGenerateCrosswordException extends Exception {
+
+	final static long serialVersionUID = -1;
+	
+	/**
+	 * Constructor
+	 */
+	public FailedToGenerateCrosswordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 * @param message
+	 */
+	public FailedToGenerateCrosswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 * @param cause
+	 */
+	public FailedToGenerateCrosswordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 * @param message
+	 * @param cause
+	 */
+	public FailedToGenerateCrosswordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructor
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public FailedToGenerateCrosswordException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
